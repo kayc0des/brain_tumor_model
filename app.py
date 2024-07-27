405465aced2d904f2e1b468c464fa99cdf75f238
kayc0des <k.boafo@alustudent.com>
2024-07-27 20:24:45 +0200

    app.py updated

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ app.add_middleware(
 )
 
 model_path = 'model/braintumor.pkl'
-model = Model()  # Instantiate the Model class
 
 
 class RetrainRequest(BaseModel):
@@ -41,31 +40,43 @@ class RetrainRequest(BaseModel):
     epochs: int
     npz_filename: str
 
+@app.get('/')
+def read_root():
+    return {"message": "Welcome to the Brain Tumor Classification Model, use '<url>/docs' for documentation"}
+
+
 @app.post('/predict')
 async def predict(file: UploadFile = File(...)):
-    # Read image file
-    image = Image.open(io.BytesIO(await file.read())).convert('RGB')
-    image = image.resize((240, 240))
-    image_data = np.asarray(image)
+    try:
+        # Load the latest model
+        model = Model()
+        model.load_model(filepath=model_path)
 
-    if image_data.shape != (240, 240, 3):
-        return {"error": "Invalid image shape. Expected (240, 240, 3)."}
+        # Read image file
+        image = Image.open(io.BytesIO(await file.read())).convert('RGB')
+        image = image.resize((240, 240))
+        image_data = np.asarray(image)
 
-    image_data = image_data / 255.0
-    image_data = image_data.reshape(1, 240, 240, 3)
+        if image_data.shape != (240, 240, 3):
+            return {"error": "Invalid image shape. Expected (240, 240, 3)."}
 
-    # Perform prediction
-    prediction = model.predict(image_data)
-    probability = prediction[0][1]
+        image_data = image_data / 255.0
+        image_data = image_data.reshape(1, 240, 240, 3)
 
-    predicted_class = "Tumor" if probability >= 0.5 else "No Tumor"
+        # Perform prediction
+        prediction = model.predict(image_data)
+        probability = prediction[0][1]
 
-    return {
-        "prediction": {
-            "class": predicted_class,
-            "probability": float(probability)
+        predicted_class = "Tumor" if probability >= 0.5 else "No Tumor"
+
+        return {
+            "prediction": {
+                "class": predicted_class,
+                "probability": float(probability)
+            }
         }
-    }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post('/retrain')
 async def retrain(request: RetrainRequest):
@@ -102,7 +113,7 @@ async def retrain(request: RetrainRequest):
             "val_accuracy": history.history['val_accuracy'][-1]
         }
 
-        cnn_model.save_model(filepath='model/braintumor2.pkl')
+        cnn_model.save_model(filepath='model/braintumor.pkl')
 
         return {"status": "Model retraining complete", "metrics": training_metrics}
     except Exception as e:
